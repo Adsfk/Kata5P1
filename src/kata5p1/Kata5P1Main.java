bf6e7b646fb07a44866049b8b1e98aa83e3506b1
Adsfk <alv_morenitu@hotmail.com>
2018-11-17 13:54:00 +0000

    KATA5P1 v2

--- src/kata5p1/Kata5P1Main.java
+++ src/kata5p1/Kata5P1Main.java
@@ -4,22 +4,42 @@ import java.sql.*;
 
 public class Kata5P1Main {
     public static void main(String[] args) {
-        SelectAll();
+        Connection con = connect();
+        createEmail(con);
+        selectAll(con);
+        try {
+            con.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
-    private static void SelectAll() {
-        String sql = "SELECT * FROM PEOPLE";
+    private static void createEmail(Connection con) {
+        String sql = "CREATE TABLE MAIL (\n" +
+                " Id integer PRIMARY KEY AUTOINCREMENT, \n" +
+                " Mail text NOT NULL);";
+        try{
+            Statement stm = con.createStatement();
+            stm.execute(sql);
+            System.out.println("Tabla creada");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
-        try(Connection con = connect()){
+    private static void selectAll(Connection con) {
+        String sql = "SELECT * FROM PEOPLE";
+        try{
             Statement stm = con.createStatement();
             ResultSet rs = stm.executeQuery(sql);
-
             while(rs.next()){
                 System.out.println(rs.getInt("id") + "\t" +
                         rs.getString("Name") + " \t" +
                         rs.getString("Apellidos") + "\t" +
                         rs.getString("Departamento") + "\t");
             }
+            stm.close();
+            rs.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
